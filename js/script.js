b95f9ad4b2a69b7366f24d54c4fba5be119864ea
Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-05-08 23:40:35 +0600

    click function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ roundScores = 0;
 activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 document.querySelector("#pscore" + activePlayer).textContent = dice;
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  if (gamePlaying) {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 100) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      gamePlaying = false;
+    } else {
+      //Next player
+      nextPlayer();
+    }
+  }
+});
