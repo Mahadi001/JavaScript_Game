bb353d7a0735e36a51bd9b7d7e756bad0b28dbb7
Mohammed Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-05-10 22:47:03 +0600

    hold_functionality

--- js/script.js
+++ js/script.js
@@ -17,20 +17,34 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 
   var diceDom = document.querySelector(".dice");
   diceDom.style.display = "block";
-  diceDom.src = "/img/" + dice + ".png";
+  diceDom.src = "img/" + dice + ".png";
 
   if (dice !== 1) {
     roundScores += dice;
     document.getElementById("pscore" + activePlayer).textContent = roundScores;
   } else {
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScores = 0;
-    document.getElementById("pscore0").textContent = "0";
-    document.getElementById("pscore1").textContent = "0";
-
-    document.querySelector(".left").classList.toggle("active");
-    document.querySelector(".right").classList.toggle("active");
-
-    document.querySelector(".dice").style.display = "none";
+    nextPlayer();
   }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  scores[activePlayer] += roundScores;
+  document.querySelector("#gscore" + activePlayer).textContent =
+    scores[activePlayer];
+  if (scores[activePlayer] >= 100) {
+    document.querySelector("#gscore" + activePlayer).textContent = "Winner";
+  }
+  nextPlayer();
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScores = 0;
+  document.getElementById("pscore0").textContent = "0";
+  document.getElementById("pscore1").textContent = "0";
+
+  document.querySelector(".left").classList.toggle("active");
+  document.querySelector(".right").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
