227d56e1d012dbcde9a452f9df94edcc25d4e7f3
Mohammed Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-05-10 23:52:08 +0600

    complete_game

--- js/script.js
+++ js/script.js
@@ -1,41 +1,40 @@
-var scores, roundScores, activePlayer;
-
-scores = [0, 0];
-roundScores = 0;
-activePlayer = 0;
-
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("gscore0").textContent = "0";
-document.getElementById("gscore1").textContent = "0";
-document.getElementById("pscore0").textContent = "0";
-document.getElementById("pscore1").textContent = "0";
+var scores, roundScores, activePlayer, gamePlaying;
+gamePlaying = true;
+newGame();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  //This line is generating random numbers from 1 to 6
-  var dice = Math.floor(Math.random() * 6) + 1;
+  if (gamePlaying) {
+    var dice = Math.floor(Math.random() * 6) + 1;
 
-  var diceDom = document.querySelector(".dice");
-  diceDom.style.display = "block";
-  diceDom.src = "img/" + dice + ".png";
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
+    diceDom.src = "img/" + dice + ".png";
 
-  if (dice !== 1) {
-    roundScores += dice;
-    document.getElementById("pscore" + activePlayer).textContent = roundScores;
-  } else {
-    nextPlayer();
+    if (dice !== 1) {
+      roundScores += dice;
+      document.getElementById(
+        "pscore" + activePlayer
+      ).textContent = roundScores;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  scores[activePlayer] += roundScores;
-  document.querySelector("#gscore" + activePlayer).textContent =
-    scores[activePlayer];
-  if (scores[activePlayer] >= 100) {
-    document.querySelector("#gscore" + activePlayer).textContent = "Winner";
+  if (gamePlaying) {
+    scores[activePlayer] += roundScores;
+    document.querySelector("#gscore" + activePlayer).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+      document.querySelector("#gscore" + activePlayer).textContent = "Winner";
+      gamePlaying = false;
+    }
+    nextPlayer();
   }
-  nextPlayer();
 });
+
+document.querySelector(".btn-new").addEventListener("click", newGame);
 
 function nextPlayer() {
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
@@ -47,4 +46,17 @@ function nextPlayer() {
   document.querySelector(".right").classList.toggle("active");
 
   document.querySelector(".dice").style.display = "none";
+}
+
+function newGame() {
+  scores = [0, 0];
+  roundScores = 0;
+  activePlayer = 0;
+
+  document.querySelector(".dice").style.display = "none";
+
+  document.getElementById("gscore0").textContent = "0";
+  document.getElementById("gscore1").textContent = "0";
+  document.getElementById("pscore0").textContent = "0";
+  document.getElementById("pscore1").textContent = "0";
 }
